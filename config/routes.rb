479ea7e87d14ed5559731852208ba4130b493802
bernard jeff igcasenza <swz-mac@SWZ-MACs-iMac.local>
2019-08-19 16:20:23 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,3 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-https://github.com/bernardjeff2525/alpha_blog_testing.git
