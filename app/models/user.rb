9ea4e80fda3079e83ea6a97f7b6cb23ded9842b5
bernard jeff igcasenza <swz-mac@SWZ-MACs-iMac.local>
2019-08-20 13:45:20 +0800

    add secure password and attribute to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
 
 
-
+  has_secure_password
   before_save { self.email = email.downcase}
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_033117) do
+ActiveRecord::Schema.define(version: 2019_08_20_053329) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_08_20_033117) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "password_digest"
   end
 
 end
