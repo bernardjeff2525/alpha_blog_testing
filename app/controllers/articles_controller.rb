bef26ad6adbf3859ab24101669071d54f301c068
bernard jeff igcasenza <swz-mac@SWZ-MACs-iMac.local>
2019-08-20 17:42:43 +0800

    Complete Authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :set_article,only:[ :edit,:update,:show,:destroy]
-
+    before_action :require_user, except:[:index,:show]
      def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
      end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit,:update,:show]
+  before_action :require_same_user,only:[:edit,:update]
   def index
     @users = User.paginate(page: params[:page],per_page:5)
   end
@@ -18,15 +19,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
 
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
-      flash[:success] = "You udpated youe account Successfully"
+      flash[:success] = "You updated your account Successfully"
       redirect_to articles_path
     else
        render 'edit'
@@ -34,7 +33,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page],per_page:5)
   end
 
@@ -44,4 +42,17 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username,:email,:password)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You must be Logged in to perform that action"
+      redirect_to root_path
+    end
+  end
+
+
 end
